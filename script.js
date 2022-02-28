3ab195584810963f04bb1ad3460470c562c198ba
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-02-28 18:22:06 +0500

    Display a Map Maker

--- script.js
+++ script.js
@@ -18,19 +18,34 @@ if (navigator.geolocation)
       const { longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-      const coords = [latitude, longitude]
+      const coords = [latitude, longitude];
 
       const map = L.map('map').setView(coords, 13);
+      // console.log(map)
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 150,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup'
+            })
+          )
+          .setPopupContent('🏃‍♂️ running')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get our position');
